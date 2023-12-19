1ccaaabccc69b7ee05822ddd6a3204b374774b7c
dbagley1 <59030213+dbagley1@users.noreply.github.com>
2023-12-19 13:58:18 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+A CSS file containing specific [Tailwind utility classes](tailwind-utility-classes.css) for projects where a build step is impractical.
+
+To limit the filesize, I only included the classes I use most often and only included the "sm" and "lg" responsive breakpoints.
+
+To add/remove classes, edit the `safelist` in `tailwind.config.js` and rebuild the file.
+
+Check out the [Tailwind safelist documentation](https://tailwindcss.com/docs/content-configuration#safelisting-classes) for more information.
+
+Tailwind preflight is not included.
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+
+let breakPoints = ['sm', 'lg'];
+// breakPoints = [];
+function p(pattern, variants) { return { pattern, variants }; }
+function bp(pattern, addVariants) { return p(pattern, [...breakPoints, addVariants]); };
+
+export default {
+  safelist: [
+    bp(/^(hidden|flex|block|grid)$/),
+    p(/flex-/),
+    bp(/^(justify|items)-[^-]+$/),
+    bp(/^grid-(cols|rows)-(1|2|3|4|5|6|7|8)$/),
+    bp(/^w-\d+\/(12|5)$/),
+    bp(/^max-w-(none|full)$/),
+    ...[150, 300, 450, 600].flatMap(x => breakPoints.map(bp => `${bp}:max-w-[${x}px]`)),
+    bp(/auto-cols-(min|max|fr|auto)/),
+    bp(/^text-(xs|sm|base|md|lg|xl|\dxl|left|right|center)$/),
+    p(/^font-/),
+    p(/^object-/),
+    bp(/^bg-((no|left|right)-)\w+$/),
+    bp(/^bg-(auto|cover|contain)$/),
+    bp(/^(w|h)-full$/),
+    p(/line-clamp-/),
+    bp(/aspect-/),
+    bp(/^(m|p)-0$/)
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "var(--font-sans)",
+        serif: "var(--font-serif)",
+        mono: "var(--font-mono)"
+      },
+    },
+  },
+  plugins: [],
+};
+
